09868e301145f0b312f79443a5bbdf3809e4d0c8
oduanhaiyang <@192.168.1.103>
2022-07-05 16:19:52 +0800

    新增conf文件

---
+++ 2022ui_autotest/conf/readconf.py
@@ -0,0 +1,15 @@
+
+
+import configparser
+
+def assignment(**kwargs):
+    # 处理前置入参如data、header
+    for k, v in kwargs.items():
+        if type(v) is dict:
+            pass
+        else:
+            if v:
+                pass
+            else:
+                pass
+    pass
