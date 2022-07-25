f38db03dbb34ef447f7b589f1f35dfcc303585bf
duanhaiyang <duansea@outlook.com>
2022-07-25 17:31:24 +0800

    新增ui-web端元素定位

--- 2022ui_autotest/web/driver_start.py
+++ 2022ui_autotest/web/driver_start.py
@@ -1,18 +1,106 @@
 
 from selenium import webdriver
-
+import unittest
 from time import sleep
-driver = webdriver.Chrome()
-driver.maximize_window()
-sleep(1)
-# driver.minimize_window()
 
-driver.get(r"http://www.baidu.com")
-# sleep(2)
-print("页面title", driver.title)
-print("当前页面网址", driver.current_url)
-# print("", driver.get_cookie())
+VisitUrl = r'http://sougou.com'
+url_baidu = r'http://www.baidu.com'
 
-# driver.find_element("su")
-driver.find_element_by_id('su').click()
-driver.quit()
+
+# def baidu():
+#     driver = webdriver.Chrome()
+#     driver.maximize_window()
+#     sleep(1)
+#     # driver.minimize_window()
+#
+#     driver.get(url_baidu)
+#     # sleep(2)
+#     print("页面title", driver.title)
+#     print("当前页面网址", driver.current_url)
+#     # print("", driver.get_cookie())
+#
+#     # driver.find_element("su")
+#     driver.find_element_by_id('su').click()
+#     driver.quit()
+
+
+class VisitSouGouByChrome(unittest.TestCase):
+
+    def setUp(self):
+        # 启动谷歌浏览器
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+
+    # def test_visitsougou(self):
+    #     # 访问搜狗首页
+    #     self.driver.get(VisitUrl)
+    #     print(self.driver.current_url)
+    #     sleep(2)
+    #     # 访问百度
+    #     self.driver.get(url_baidu)
+    #     print(self.driver.current_url)
+    #     # 返回上一次访问过的搜狗首页
+    #     self.driver.back()
+    #     sleep(1)
+    #     self.driver.forward()  # 再次回到百度首页
+    #     self.driver.refresh()  # 刷新当前页面
+    #     sleep(1)
+    #     position = self.driver.get_window_position()
+    #     print("坐标：", position)
+    #     self.driver.set_window_position(y=200, x=400)
+    #     # 设置浏览器的位置后，再次获取浏览器的位置信息
+    #     print("设置后：", self.driver.get_window_position())
+    #
+    # def test_getTitle(self):
+    #     # 获取页面的属性值
+    #     self.driver.get(url_baidu)
+    #     print(self.driver.current_url)
+    #     title = self.driver.title
+    #     print("title:", title)
+
+    def test_operateWindowHandle(self):
+        self.driver.get(url_baidu)
+        # 获取当前窗口的句柄
+        now_handle = self.driver.current_window_handle
+        print("当前窗口的句柄：", now_handle)
+        search = self.driver.find_element_by_id('kw')
+        search.send_keys("w3cschool")
+        search_btn = self.driver.find_element_by_id('su')
+        search_btn.click()  # 点击搜索
+        sleep(2)
+        w3c_door = self.driver.find_element_by_xpath('//*[@id="3"]//a')
+        w3c_door.click()
+        sleep(2)
+        # 获取窗口所有句柄
+        all_handles = self.driver.window_handles
+        print("所有的句柄-最后一个：", self.driver.window_handles[-1])
+        for handle in all_handles:
+            if handle != now_handle:
+                print("输出当前不一致的句柄：", handle)
+
+        self.driver.switch_to_window(handle)
+        self.driver.find_element_by_link_text('HTML').click()
+        sleep(2)
+        self.driver.close()  # 关闭当前的窗口
+        sleep(2)
+        # 打印主窗口的句柄
+        print('主窗口的句柄：', now_handle)
+        self.driver.switch_to_window(now_handle)
+        sleep(2)
+        print('返回到百度的首页')
+        search.clear()
+        search.send_keys('光荣之路自动化测试')
+        search_btn.click()
+        sleep(5)
+        print("success")
+
+
+
+
+    def tearDown(self):
+        # 关闭浏览器
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
